c83625a9a77ac8e4234cc9e935d2bac84c8b13b3
Hubert Pomorski <hubert.pomorski@apaczka.pl>
2023-12-28 02:25:26 +0100

    handle post request from react

--- src/Movies/Communication/Controller/IndexController.php
+++ src/Movies/Communication/Controller/IndexController.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Movies\Communication\Controller;
 
 use App\Movies\Business\MoviesFacadeInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+use Symfony\Component\HttpKernel\Log\Logger;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -22,8 +25,16 @@ class IndexController extends AbstractController
      * @return JsonResponse
      */
     #[Route('/api/projects/recommendations')]
-    public function number(MoviesFacadeInterface $moviesFacade, Request $request): JsonResponse
+    public function recommendation(MoviesFacadeInterface $moviesFacade, Request $request): JsonResponse
     {
         return new JsonResponse($moviesFacade->getRecommendation($request->get('type')));
+    }
+
+    #[Route('/api/projects/enroll', name: 'enroll')]
+    public function enroll(Request $request, LoggerInterface $logger): JsonResponse
+    {
+        $requestData = json_decode($request->getContent(), true);
+
+        return new JsonResponse($requestData);
     }
 }
